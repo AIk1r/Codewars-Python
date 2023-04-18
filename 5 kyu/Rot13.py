ada002dca607b6bcd17a0ede1139252f88493e04
AIk1r <111125825+AIk1r@users.noreply.github.com>
2023-04-18 21:13:35 +0700

    Create Rot13.py

---
+++ 5 kyu/Rot13.py
@@ -0,0 +1,5 @@
+def rot13(message):
+    r13 = str.maketrans(
+    'ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz',
+    'NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm')
+    return message.translate(r13)
