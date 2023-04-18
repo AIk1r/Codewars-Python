ac1e8189762489474ee9dda230a16fbb35d72329
AIk1r <111125825+AIk1r@users.noreply.github.com>
2023-04-18 21:12:32 +0700

    Delete Interview Question(easy).py

--- 7-kyu/Interview Question(easy).py
+++
@@ -1,6 +0,0 @@
-def get_strings(city):
-    d = {}
-    for i in city.lower():
-        if i.isalpha():
-            d[i] = '*' * city.lower().count(i)
-    return(','.join(k+':'+ v for k, v in d.items()))
