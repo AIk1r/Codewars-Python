94a43168afe806a62bbd169a6699968f5d6ac1e0
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-06-02 21:25:38 +0700

    Create John and Ann sign up for Codewars.py

---
+++ 5 kyu/John and Ann sign up for Codewars.py
@@ -0,0 +1,19 @@
+def j_a(n):
+    j = [0]
+    a = [1]
+    for i in range(1, n):
+        j.append((i - a[j[i-1]])) 
+        a.append((i - j[a[i-1]]))
+    return j, a
+
+def john(n):
+    return j_a(n)[0]
+    
+def ann(n):
+    return j_a(n)[1]
+    
+def sum_john(n):
+    return sum(john(n))
+    
+def sum_ann(n):
+    return sum(ann(n))
